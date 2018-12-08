a69de97160215bcaa54d1c155f2663a6388ff343
Paul <paul.raestad@gmail.com>
2018-12-08 12:12:48 +0100

    fibdiv virker

--- hlek.hs
+++ hlek.hs
@@ -6,13 +6,15 @@ import System.IO
 doubleMe x = x + x
 
 ----------------------------------------------------------
-fib = 1:1:[a + b | (a, b) <- zip fib (tail fib)]
+-- fib :: RealFloat  => [a]
+fib = 1.0:1.0:[a + b | (a, b) <- zip fib (tail fib)]
 fib30 = fib !! 30
 
-fibdiv' :: (Num a) => [a] -> [a]
+fibdiv' :: RealFloat a => [a] -> [a]
 fibdiv' [] = []
-fibdiv' (x:[xs]) = [x+head [xs]] ++ fibdiv' [xs]
-fibdiv' [e] = [e]
+fibdiv' [e] = []
+fibdiv' (x:xs) = [head xs/x] ++ fibdiv' xs
+
 
 -----------------------------------------------------
 rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24] 
