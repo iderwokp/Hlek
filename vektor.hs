55362f29265363e801d05b8c076fbc9a597088d5
Ubuntu <pilal@pilbuntu16.pilbuntu168118.f6.internal.cloudapp.net>
2018-12-31 22:29:01 +0100

    vektor ok litt

--- vektor.hs
+++ vektor.hs
@@ -1,15 +1,17 @@
 
-data Vektor2d = Vektor2d {x::Double, y::Double} Deriving(Show, Eq)
+data Vektor2d  = Vektor2d Double Double deriving(Show, Eq)
 
 v2dx :: Vektor2d -> Double
-v2dx Vektor2d(x _) = x
+v2dx (Vektor2d x _) = x
 
 v2dy :: Vektor2d -> Double
-v2dy Vektor2d(_ y) = y
+v2dy (Vektor2d _ y) = y
 
-v2dAdd :: (Num t) => Vektor2d t -> Vektor2d t -> Vektor2d t
+-- v2dAdd :: (Num t) => Vektor2d t -> Vektor2d t -> Vektor2d t 
 v2dAdd (Vektor2d x y) (Vektor2d x1 y1) =  Vektor2d (x+x1) (y+y1)
 
 
+v1 = (Vektor2d 2 3)
+v2 = (Vektor2d 1 1)
 
 
