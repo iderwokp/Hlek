39e1ef810349cb09a6800f95f649b8d18f8b68c8
Paul Raestad <paulor@online.no>
2019-01-03 23:37:28 +0100

    vokaler og konsonanter

--- vowel.hs
+++ vowel.hs
@@ -1,13 +1,21 @@
-
+import Data.Char
 
 ll = length.filter (`elem` "aeiouy")
 -- Brukes slik: ll "navn"
 
-vow_len :: String -> int
+vow_len :: String -> Int
 vow_len [] = 0
-vow_len s = length. isVow 
+vow_len s =   length $ vows s
     
 
-isVow :: [a]-> [Bool]
-isVow [] = [False]
-isVow (x:xs) = x `elem` "aeiouy" :isVow xs 
+vows :: [Char]-> [Char]
+vows [] = []
+vows s = filter ( `elem` "aeiouyæøå" ) $ map toLower s
+
+kons_len :: String -> Int
+kons_len [] = 0
+kons_len s =   length $ kons s
+
+kons :: [Char]-> [Char]
+kons [] = []
+kons s = filter (`notElem` "aeiouyæøå" ) $ map toLower s
