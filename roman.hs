2608dd8c572e81f6ad11a3bae4e3f1b8af7f12ce
Ubuntu <pilal@pilbuntu16.pilbuntu168118.f6.internal.cloudapp.net>
2019-05-05 15:43:20 +0200

    Mer roman

--- roman.hs
+++ roman.hs
@@ -1,14 +1,28 @@
 
-romToInt :: String -> Int
-romToInt [] = 0
+romToInt ::Char-> Int
+-- romToInt [] = 0
 romToInt s 
-    | s == "I" = 1
-    | s == "V" = 5
-    | s == "X" = 10
-    | s == "L" = 50
-    | s == "C" = 100
-    | s == "D" = 500
-    | s == "M" = 1000
+    | s == 'I' = 1
+    | s == 'V' = 5
+    | s == 'X' = 10
+    | s == 'L' = 50
+    | s == 'C' = 100
+    | s == 'D' = 500
+    | s == 'M' = 1000
     | otherwise  = 0
+
+getThunk :: [Char] -> [Char]
+getThunk [] = []
+getThunk [x] = [x]
+getThunk (x:y:xs) 
+    |romToInt x <= romToInt y = x : getThunk (y:xs)
+    |otherwise = [x]
+
+thunks :: String -> [String]
+thunks [] = []
+thunks xs = getThunk xs : thunks ( drop ( length $ getThunk xs )  xs ) 
+
+
+
 
     
