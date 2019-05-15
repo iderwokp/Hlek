6a4ac64946bbcd86fd58f43a47555d269209861d
Ubuntu <pilal@pilbuntu16.pilbuntu168118.f6.internal.cloudapp.net>
2019-05-15 22:02:50 +0200

    oppgaven

--- kladd.hs
+++ kladd.hs
@@ -15,9 +15,9 @@ enDeltPa :: (Fractional a, Eq a) => a -> Maybe a
 enDeltPa 0.0 = Nothing
 enDeltPa t = Just (1 / (t*(t+1)))
 
-oppgaven :: (Num a, Fractional a, Eq a) =>  Maybe a ->  [Maybe a]
-oppgaven Nothing = [Nothing]
-oppgaven x = enDeltPa x : oppgaven (addJust(x + 1))
+oppgaven :: (Num a, Fractional a, Eq a) =>  a ->  [Maybe a]
+-- oppgaven Nothing = [Nothing]
+oppgaven x = enDeltPa x : oppgaven (x + 1)
 
 addJust :: Num a => Maybe a -> a -> Maybe a
 addJust (Just x) y = Just (x+y)
