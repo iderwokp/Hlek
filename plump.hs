729cff5eccb9eb1e6045b70fc623550e8115c49a
PilalDev <paul.raestad@gmail.com>
2021-01-07 17:20:37 +0100

    plump

---
+++ plump.hs
@@ -0,0 +1,8 @@
+
+--plump linje = read.last.words $ linje::Int
+plump  = read (last.words)::Int
+--let tekst = "we swe 45\nuee wqw 55"
+--map plump.lines $ tekst
+--foldl (+) 0 $ map plump.lines $ tekst
+
+
